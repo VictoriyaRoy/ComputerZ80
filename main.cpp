20262ba4d4e86b675c38ef702e043388c9f66068
bohdaholas <73237406+bohdaholas@users.noreply.github.com>
2021-12-18 19:56:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include <Arduino.h>
+#include <stdio.h>
 
+// Z80 program byte array
 byte data[] = {0x3E, 0x00, 0xD3, 0xFF, 0x06, 0x00, 0x10, 0xFE, 0x2F, 0xD3, 0xFF, 0x10, 0xFE, 0x18, 0xF1};
-// byte data[] = {0x3E, 0x01, 0xD3, 0xFF, 0xC3, 0x00, 0x00};
-// byte data[] = {0xD3, 0xFF, 0x06, 0x20, 0x10, 0xFE, 0x2F, 0xD3, 0xFF, 0x06, 0x20, 0x10, 0xFE, 0x18, 0xF1};
-// byte data[] = {0xD3, 0xFF, 0x06, 0x00, 0x10, 0xFE, 0x10, 0xFE, 0x2F, 0xD3, 0xFF, 0x10, 0xFE, 0x18, 0xF1};
+
+// PORTD - data pins (Arduino pins 0-7)
+// PORTB - address pins (Arduino pins 8-11)
+// PORTB (4) - OE (Arduino pin 12)
+// PORTB (5) - CS (Arduino pin 13)
 
 void setup() {
-	DDRD = 0x00; // data pins 0-7
-	DDRB = 0x00; // address pins 8-11 
-	PORTB = 0x30; 
+   Serial.begin(9600);
+   DDRD = 0x00; // set PORTD in input mode
+   DDRB = 0x00; // set PORTB in input mode
+   PORTB = 0x30; // set OE and CS in input pullup mode
+   pinMode(A0, OUTPUT);
+}
+
+char str[64];
+void printAllBuses() {
+  int oe_cs = (digitalRead(12) << 1) | (digitalRead(13));
+  if(!oe_cs){
+      sprintf(str, "OE_CS:%d, CLK:%d, A:%x D:%x",oe_cs, digitalRead(A0), PINB, PORTD);
+      Serial.println(str);
+  }
 }
 
 void loop() {
-	PORTD = data[PINB & 0xF];
-	DDRD = PINB & 0x30 ? 0x0 : 0xFF;
+  delay(100);
+  digitalWrite(A0, !digitalRead(A0));
+  delay(10);
+  printAllBuses();
+  PORTD = data[PINB & 0xF]; // write data to PORTD
+  DDRD = PINB & 0x30 ? 0x0 : 0xFF; // set mode of PORTD
 }
