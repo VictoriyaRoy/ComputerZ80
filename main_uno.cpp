165f345525e44dbf76f2e29179cc2d6dd0f3792f
VictoriyaRoy <44781809+VictoriyaRoy@users.noreply.github.com>
2022-01-14 18:14:23 +0200

    Update main_uno.cpp

--- main_uno.cpp
+++ main_uno.cpp
@@ -1,3 +1,5 @@
+//For Arduino Uno
+
 #include <Arduino.h>
 #include <stdio.h>
 
@@ -10,27 +12,16 @@ byte data[] = {0x3E, 0x00, 0xD3, 0xFF, 0x06, 0x00, 0x10, 0xFE, 0x2F, 0xD3, 0xFF,
 // PORTB (5) - CS (Arduino pin 13)
 
 void setup() {
-   Serial.begin(9600);
    DDRD = 0x00; // set PORTD in input mode
    DDRB = 0x00; // set PORTB in input mode
    PORTB = 0x30; // set OE and CS in input pullup mode
    pinMode(A0, OUTPUT);
 }
 
-char str[64];
-void printAllBuses() {
-  int oe_cs = (digitalRead(12) << 1) | (digitalRead(13));
-  if(!oe_cs){
-      sprintf(str, "OE_CS:%d, CLK:%d, A:%x D:%x",oe_cs, digitalRead(A0), PINB, PORTD);
-      Serial.println(str);
-  }
-}
-
 void loop() {
   delay(100);
   digitalWrite(A0, !digitalRead(A0));
   delay(10);
-  printAllBuses();
   PORTD = data[PINB & 0xF]; // write data to PORTD
   DDRD = PINB & 0x30 ? 0x0 : 0xFF; // set mode of PORTD
 }
